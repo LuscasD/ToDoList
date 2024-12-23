5dda10a121f07f5def62f17ad429fa1cfc4e7fc2
LuscasD <73210639+LuscasD@users.noreply.github.com>
2024-12-22 23:15:48 -0300

    AdjustButtons

--- ToDoListProject/app/src/main/java/com/example/todolistandroid/MainActivity.kt
+++ ToDoListProject/app/src/main/java/com/example/todolistandroid/MainActivity.kt
@@ -5,10 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolistandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+     private lateinit var binding: ActivityMainBinding
+     private  lateinit var  taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener{
+
+            newTask().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this)
+        {
+            binding.taskName.text = String.format("Nome da tarefa: %s", it)
+        }
+        taskViewModel.desc.observe(this)
+        {
+            binding.taskDesc.text = String.format("Nome da tarefa: %s", it)
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
